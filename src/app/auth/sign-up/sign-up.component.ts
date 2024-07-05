d8bdde491ad5e7bd2d2e8a1d2f421ba37cc09476
Issam M <mejdoubi.issam@gmail.com>
2024-07-05 17:26:58 +0100

    sign up form validation

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,33 +1,50 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { AbstractControlOptions, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
-import { AuthService } from '../../auth.service';
 import { environment } from '../../../environments/environment';
 import { take } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 interface AuthRegisterResponse {
   error?: string;
   message?: string;
 }
 
-
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [FormsModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+  signupForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    repeatPassword: new FormControl('', [Validators.required])
+  }, {
+    validators: [this.checkPasswords]
+  } as AbstractControlOptions);
+
   email: string = '';
   password: string = '';
   repeatPassword: string = '';
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) {
+  }
 
   signUp(): void {
     console.log('Signing up...', this.email, this.password, this.repeatPassword);
+
+    if (!this.signupForm.valid) {
+      console.error('Form is invalid');
+      return;
+    }
+
     this.http.post<AuthRegisterResponse>(`${environment.apiEndpoint}/api/register`, { email: this.email, password: this.password, repeatPassword: this.repeatPassword })
       .pipe(take(1))
       .subscribe((response: AuthRegisterResponse) => {
@@ -39,5 +56,23 @@ export class SignUpComponent {
         console.log('Signed up:', response.message);
         this.router.navigate(['/sign-in']);
     });
+  }
+
+  checkPasswords(group: FormGroup) {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('repeatPassword')?.value;
+    return password === confirmPassword ? null : { notSame: true };
+  }
+
+  get emailControl() {
+    return this.signupForm.get('email');
+  }
+
+  get passwordControl() {
+    return this.signupForm.get('password');
+  }
+
+  get repeatPasswordControl() {
+    return this.signupForm.get('repeatPassword');
   }
 }
