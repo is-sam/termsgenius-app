4549a6dbf3b2426b271e0637779604c11787f4b1
Issam M <mejdoubi.issam@gmail.com>
2024-07-25 17:00:54 +0100

    Profile & logout menu

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit, Signal, signal } from '@angular/core';
+import { Component, OnInit, Signal, signal, ViewChild } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
 import { StyleClassModule } from 'primeng/styleclass';
 import { AuthService } from '../../auth/auth.service';
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SidebarModule, ButtonModule, AvatarModule, StyleClassModule, RouterLink, RouterLinkActive],
+  imports: [SidebarModule, ButtonModule, AvatarModule, StyleClassModule, RouterLink, RouterLinkActive, MenuModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent implements OnInit {
   userEmail = signal('');
+  profileMenuVisible = false;
+  items: Array<MenuItem> = [
+    { label: 'Profile', icon: 'pi pi-fw pi-user', routerLink: '/profile' },
+    { label: 'Sign Out', icon: 'pi pi-fw pi-sign-out', routerLink: '/sign-out' },
+  ];
 
   constructor(private authService: AuthService) {}
 
