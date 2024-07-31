be1c3f9cbb9324f1d970fa98d4eb36d307a48109
Issam M <mejdoubi.issam@gmail.com>
2024-07-31 15:31:44 +0100

    Dashboard data

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from '../../services/dashboard.service';
+import { CommonModule } from '@angular/common';
+import { SkeletonModule } from 'primeng/skeleton';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SkeletonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  constructor(public dashboardService: DashboardService) {}
 
+  ngOnInit() {
+    console.log('Dashboard loaded');
+    this.dashboardService.loadData();
+  }
 }
