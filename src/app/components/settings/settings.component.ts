6834633abc16e166141d877ff1b4b0454eba07c5
Issam M <mejdoubi.issam@gmail.com>
2024-08-02 12:04:15 +0100

    settings page (wip)

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ToggleButtonModule } from 'primeng/togglebutton';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ToggleButtonModule, FormsModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
+  darkMode = false;
 
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+    // localStorage.setItem('darkmode', this.darkMode.toString());
+    // console.log('Dark mode toggled', this.darkMode);
+  }
 }
