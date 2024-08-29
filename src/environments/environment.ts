728542744889d5eba7bfdb82a80676a53dc3f52f
Issam M <mejdoubi.issam@gmail.com>
2024-08-29 16:29:22 +0100

    set new prod google client

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiEndpoint: 'https://api.termzai.com',
-    googleClientId: '859716614206-a9k55amtc4gdpb93ian4ait6t6b0aep3.apps.googleusercontent.com',
+    googleClientId: '859716614206-odpd3lnha1f0c98mitbspblo16ettm8d.apps.googleusercontent.com',
 };
